287216703bd36197ca18ca75bb0392b70c7dbed9
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-05-10 21:12:15 -0300

    style
    Profile Heads components maked

--- src/components/pages/Profile/HeaderProfile.jsx
+++ src/components/pages/Profile/HeaderProfile.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import {HeaderPro} from './Profile.style'
-const HeaderProfile = () => {
+import React from "react";
+import { ImageProfile } from "../../Header.style";
+import { HeaderPro, Name, Professional, WrapperInfs } from "./Profile.style";
+const HeaderProfile = ({ name, img, professional }) => {
   return (
-      <HeaderPro>
-          
+    <HeaderPro>
+      <WrapperInfs>
+        <ImageProfile src={img} />
+        <Name>{name}</Name>
+        <Professional>{professional}</Professional>
+      </WrapperInfs>
     </HeaderPro>
-  )
-}
+  );
+};
 
-export default HeaderProfile
+export default HeaderProfile;
--- src/components/pages/Profile/Profile.style.jsx
+++ src/components/pages/Profile/Profile.style.jsx
@@ -1,18 +1,31 @@
 import styled from "styled-components";
-
+import { Tittle } from "../../../Global/GeneralComponents";
 export const Wrapper = styled.div`
   background-color: ${(prop) => prop.theme.colors.blackSecondary};
   width: 97%;
   height: 90%;
-  border-radius: .785rem;
+  border-radius: 0.785rem;
 `;
 export const Content = styled.section`
-    width: 100%;
-`
+  width: 100%;
+`;
 export const HeaderPro = styled.header`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const WrapperInfs = styled.div`
+  width: 50%;
+  display: grid;
+`
+export const imgProfile = styled.img`
+  width: 50px;
+`
+export const Name = styled(Tittle)`
+  font-weight: 400;
+`
+export const Professional = styled.span`
+  font-weight: 300;
 `
